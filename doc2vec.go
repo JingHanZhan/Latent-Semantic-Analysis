41acc5ed1bc1529ddf7abe479c3ceca9854feecd
JingHanZhan <41484306+JingHanZhan@users.noreply.github.com>
2019-01-05 15:28:56 +0800

    Add files via upload
    文章向量化，下一步完成TFIDF計算

---
+++ doc2vec.go
@@ -0,0 +1,440 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+	"unsafe"
+)
+
+//HASHSIZE is hashtable size
+//const HASHSIZE = 100000
+
+var (
+	i         = 0
+	collision = 0
+	docNumber = 0
+)
+
+func main() {
+	checkParameter()
+	clearOutputFile()
+	start := time.Now()
+	var (
+		nodeTable = make([]*node, *args.h) //[*args.m]*node
+		keyTable  []*key
+	)
+	fmt.Println("cap(nodeTable):", unsafe.Sizeof(node{"1", 1, nil}))
+	//fmt.Println("*args.m:", *args.m)
+	creatBasicTable(nodeTable[:], keyTable)
+	fmt.Println("How many collision:", collision)
+	clearTableCount(nodeTable[:])
+	doc2vec(nodeTable[:], keyTable)
+
+	/* 下面兩條應該包進去doc2vec function內 */
+	// outputNodeTable(nodeTable[:])
+	// outputVector(nodeTable[:])
+
+	//fmt.Println("test nodetable:", nodeTable[0].count)
+	fmt.Println("time:", time.Now().Sub(start).Seconds())
+	//test()
+}
+
+func test() {
+	//test2
+	hashFile, err := os.Open("test2.txt")
+	if err != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer hashFile.Close()
+	scanner := bufio.NewReader(hashFile)
+	var stringBuf string
+	for i := 0; i < 3; i++ {
+		stringBuf, _ = scanOneLine(scanner)
+	}
+	hashNumber := hash(&stringBuf)
+	fmt.Println("test function => hashNumber:", hashNumber)
+
+	//test3
+	hashFile2, err2 := os.Open("test3.txt")
+	if err2 != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer hashFile2.Close()
+	scanner2 := bufio.NewReader(hashFile2)
+	var stringBuf2 string
+	for j := 0; j < 3; j++ {
+		stringBuf2, _ = scanOneWord(scanner2)
+	}
+	stringBuf2 = stringBuf2 + "\r\n"
+	hashNumber2 := hash(&stringBuf2)
+	fmt.Println("test function => hashNumber2:", hashNumber2)
+	fmt.Println(stringBuf)
+	fmt.Println(stringBuf2)
+}
+
+var args struct {
+	m *uint64
+	s *uint64
+	h *uint64
+}
+
+type node struct {
+	//keypos uint
+	key   string
+	count int
+	next  *node
+}
+
+type key struct {
+	keypos *string
+	count  *int
+}
+
+func doc2vec(nodeTable []*node, keyTable []*key) {
+	fmt.Println("Start doc to vector ...")
+	hashFile, err := os.Open("test.txt")
+	if err != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer hashFile.Close()
+	scanner := bufio.NewReader(hashFile)
+	stringBuf, readStringErr := scanOneWord(scanner)
+	//stringBuf = stringBuf + "\n"
+
+	for readStringErr == nil {
+		if strings.Contains(stringBuf, "\n") {
+			s := strings.Split(stringBuf, "\r\n")
+			// fmt.Println(s)
+			//fmt.Println("contain change line:", stringBuf)
+			stringBuf = s[0]
+
+			// 去除空格
+			stringBuf = strings.Replace(stringBuf, " ", "", -1)
+			// 去除换行符
+			stringBuf = strings.Replace(stringBuf, "\n", "", -1)
+
+			if uint64(len(stringBuf)) <= (*args.s + 2) {
+				hashNumber := hash(&stringBuf)
+				nodeTable[hashNumber], keyTable = addVecWord(nodeTable[hashNumber], stringBuf, keyTable)
+			}
+			outputNodeTable(nodeTable[:])
+			outputVector(nodeTable[:])
+			clearTableCount(nodeTable[:])
+			//stringBuf, readStringErr = scanOneWord(scanner)
+			stringBuf = s[1]
+			docNumber++
+		} else {
+			// 去除空格
+			stringBuf = strings.Replace(stringBuf, " ", "", -1)
+			if uint64(len(stringBuf)) <= (*args.s + 2) {
+				hashNumber := hash(&stringBuf)
+				nodeTable[hashNumber], keyTable = addVecWord(nodeTable[hashNumber], stringBuf, keyTable)
+			}
+			stringBuf, readStringErr = scanOneWord(scanner)
+		}
+	}
+}
+
+/* 原本的BasicTable funcion */
+// func creatBasicTable(nodeTable []*node, keyTable []*key) {
+// 	hashFile, err := os.Open(os.Args[1])
+// 	if err != nil {
+// 		fmt.Println("Open file fail !")
+// 	}
+// 	defer hashFile.Close()
+// 	scanner := bufio.NewReader(hashFile)
+// 	stringBuf, readStringErr := scanOneLine(scanner)
+// 	for readStringErr == nil {
+// 		if uint64(len(stringBuf)) <= (*args.s + 2) {
+// 			hashNumber := hash(&stringBuf)
+// 			nodeTable[hashNumber], keyTable = addWord(nodeTable[hashNumber], stringBuf, keyTable)
+// 		}
+// 		stringBuf, readStringErr = scanOneLine(scanner)
+// 	}
+// }
+
+/* 改版BasicTable，施工中 */
+func creatBasicTable(nodeTable []*node, keyTable []*key) {
+	hashFile, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer hashFile.Close()
+	scanner := bufio.NewReader(hashFile)
+	stringBuf, readStringErr := scanOneWord(scanner)
+
+	// 去除空格
+	stringBuf = strings.Replace(stringBuf, " ", "", -1)
+	// 去除换行符
+	stringBuf = strings.Replace(stringBuf, "\n", "", -1)
+
+	for readStringErr == nil {
+		if uint64(len(stringBuf)) <= (*args.s + 2) {
+			hashNumber := hash(&stringBuf)
+			nodeTable[hashNumber], keyTable = addWord(nodeTable[hashNumber], stringBuf, keyTable)
+		}
+		stringBuf, readStringErr = scanOneWord(scanner)
+
+		// 去除空格
+		stringBuf = strings.Replace(stringBuf, " ", "", -1)
+		// 去除换行符
+		stringBuf = strings.Replace(stringBuf, "\n", "", -1)
+
+	}
+}
+
+func clearTableCount(nodeTable []*node) {
+	for n := 0; n < len(nodeTable); n++ {
+		for p := nodeTable[n]; p != nil; p = p.next {
+			p.count = 0
+			//nodeTableFile.WriteString(fmt.Sprintf("%d %s", p.count, p.key))
+		}
+	}
+}
+
+func checkParameter() {
+	for i := 1; i < len(os.Args); i++ {
+		switch os.Args[i] {
+		case "-m":
+			parmBuf, err := strconv.ParseUint(os.Args[i+1], 10, 64)
+			if err != nil {
+				fmt.Println("Parameter m error: ", os.Args[i+1])
+			} else {
+				args.m = &parmBuf
+				fmt.Println("使用參數:m = ", *args.m)
+			}
+		case "-s":
+			parmBuf, err := strconv.ParseUint(os.Args[i+1], 10, 64)
+			if err != nil {
+				fmt.Println("Parameter s error: ", os.Args[i+1])
+			} else {
+				args.s = &parmBuf
+				fmt.Println("使用參數:s = ", *args.s)
+			}
+		case "-h":
+			parmBuf, err := strconv.ParseUint(os.Args[i+1], 10, 64)
+			if err != nil {
+				fmt.Println("Parameter h error: ", os.Args[i+1])
+			} else {
+				args.h = &parmBuf
+				fmt.Println("使用參數:h = ", *args.h)
+			}
+		}
+	}
+}
+
+/* 計算單字詞頻，建立basic node table，遇到新的單字擴增*/
+func addWord(newWord *node, stringBuf string, keyTable []*key) (*node, []*key) {
+	memoryLimit()
+	i++
+	fmt.Printf("Add word : %d\r", i)
+	if newWord == nil {
+		newWord = &node{stringBuf, 1, nil}
+		keyTable = append(keyTable, &key{&newWord.key, &newWord.count})
+		return newWord, keyTable
+	}
+
+	for w := newWord; w != nil; w = w.next {
+		if w.key == stringBuf {
+			w.count++
+			return newWord, keyTable
+		}
+		if w.next == nil {
+			collision++
+			newNode := &node{stringBuf, 1, nil}
+			keyTable = append(keyTable, &key{&newNode.key, &newNode.count})
+			w.next = newNode
+			return newWord, keyTable
+		}
+	}
+	return newWord, keyTable
+}
+
+/* 計算單字詞頻，放入原本basic node table，但遇到新的單字不增加向量維度 */
+func addVecWord(newWord *node, stringBuf string, keyTable []*key) (*node, []*key) {
+	memoryLimit()
+	i++
+	//fmt.Printf("Add word : %d\r", i)
+	if newWord == nil {
+		// newWord = &node{stringBuf, 1, nil}
+		// keyTable = append(keyTable, &key{&newWord.key, &newWord.count})
+		return newWord, keyTable
+	}
+
+	for w := newWord; w != nil; w = w.next {
+		if w.key == stringBuf {
+			w.count++
+			return newWord, keyTable
+		}
+		if w.next == nil {
+			// collision++
+			// newNode := &node{stringBuf, 1, nil}
+			// keyTable = append(keyTable, &key{&newNode.key, &newNode.count})
+			// w.next = newNode
+			return newWord, keyTable
+		}
+	}
+	return newWord, keyTable
+}
+
+func printNodeTable(nodeTable []*node) {
+	for n := 0; n < len(nodeTable); n++ {
+		for p := nodeTable[n]; p != nil; p = p.next {
+			fmt.Println(p)
+		}
+	}
+}
+
+func outputVector(nodeTable []*node) {
+	fmt.Println("Output vector...")
+	vecFile, err := os.OpenFile("doc2vec.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer vecFile.Close()
+	for n := 0; n < len(nodeTable); n++ {
+		for p := nodeTable[n]; p != nil; p = p.next {
+			vecFile.WriteString(fmt.Sprintf("%d ", p.count))
+		}
+	}
+	vecFile.WriteString(fmt.Sprintf("\n"))
+}
+
+/* 原始outputTable，會覆蓋原本檔案內容*/
+// func outputNodeTable(nodeTable []*node) {
+// 	fmt.Println("Output node table...")
+// 	nodeTableFile, err := os.Create("NodeTable.txt")
+// 	if err != nil {
+// 		fmt.Println("Open file fail !")
+// 	}
+// 	for n := 0; n < len(nodeTable); n++ {
+// 		for p := nodeTable[n]; p != nil; p = p.next {
+// 			nodeTableFile.WriteString(fmt.Sprintf("%d %s", p.count, p.key))
+// 		}
+// 	}
+// }
+
+/* 改版outputTable，若檔案不存在則建立，檔案已存在則續寫，不會覆蓋原本內容*/
+func outputNodeTable(nodeTable []*node) {
+	fmt.Println("Output node table...")
+	//nodeTableFile, err := os.Create("NodeTable.txt")
+	nodeTableFile, err := os.OpenFile("NodeTable.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer nodeTableFile.Close()
+	for n := 0; n < len(nodeTable); n++ {
+		for p := nodeTable[n]; p != nil; p = p.next {
+			nodeTableFile.WriteString(fmt.Sprintf("%d%s ", p.count, p.key))
+		}
+	}
+	nodeTableFile.WriteString(fmt.Sprintf("\n"))
+}
+
+func clearOutputFile() {
+	fmt.Println("Clear NodeTable.txt & doc2vec.txt ...")
+	nodeTableFile, err := os.Create("NodeTable.txt")
+	vecFile, err2 := os.Create("doc2vec.txt")
+	//nodeTableFile, err := os.OpenFile("NodeTable.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil || err2 != nil {
+		fmt.Println("Open file fail !")
+	}
+	defer nodeTableFile.Close()
+	defer vecFile.Close()
+}
+
+func outputSortTable(keyTable []*key) {
+	fmt.Println("Output sort table...")
+	SortTableFile, err := os.Create("SortTable.txt")
+	if err != nil {
+		fmt.Println("Open file fail !")
+	}
+	for k := len(keyTable) - 1; k >= 0; k-- {
+		SortTableFile.WriteString(fmt.Sprintf("%d %s", *keyTable[k].count, *keyTable[k].keypos))
+	}
+}
+
+func hash(a *string) uint64 {
+	s := []byte(*a)
+	//fmt.Println("hash number:", *a, s)
+	var hashval uint64
+	for i := 0; i < len(s); i++ {
+		hashval = uint64(s[i]) + 131*hashval
+	}
+	return hashval % *args.h
+}
+
+func scanOneLine(scanner *bufio.Reader) (string, error) {
+	stringBuf, readStringErr := scanner.ReadString('\n')
+	//fmt.Println("scanOneLine => stringBuf:", stringBuf)
+	return stringBuf, readStringErr
+}
+
+func scanOneWord(scanner *bufio.Reader) (string, error) {
+	stringBuf, readStringErr := scanner.ReadString(' ')
+	//stringBuf = strings.Replace(stringBuf, " ", "", -1)
+	//fmt.Println("scanOneWord => stringBuf:", stringBuf)
+	//s := strings.Split(stringBuf, "")
+	//fmt.Println(stringBuf)
+	return stringBuf, readStringErr
+}
+
+func heapsort(array []*key) {
+	fmt.Println("Sorting...")
+	for i := (len(array)/2 - 1); i >= 0; i-- {
+		maxheap(array, i, len(array)-1)
+	}
+	// heapify the array into a max-heap
+	for i := len(array) - 1; i > 0; i-- {
+		array[0], array[i] = array[i], array[0]
+		maxheap(array, 0, i-1)
+	}
+}
+
+func maxheap(array []*key, start int, end int) {
+	memoryLimit()
+	left := start*2 + 1
+	right := left + 1
+	if left > end {
+		return
+	}
+	var tmp = left
+	if right <= end && *array[right].count > *array[left].count {
+		tmp = right
+	}
+	if *array[tmp].count > *array[start].count {
+		//fmt.Println(start, end, array)
+		array[start].keypos, array[tmp].keypos = array[tmp].keypos, array[start].keypos
+		array[start].count, array[tmp].count = array[tmp].count, array[start].count
+		maxheap(array, tmp, end)
+	}
+}
+
+//PrintMemUsage print memory usage
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func memoryLimit() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	if bToMb(m.Sys) >= *args.m {
+		fmt.Println("Out of the memory limit")
+		os.Exit(1)
+	}
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
